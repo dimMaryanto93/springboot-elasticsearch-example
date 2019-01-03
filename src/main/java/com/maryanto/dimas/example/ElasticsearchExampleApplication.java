5d677df4f2ca213f8bc0c9bfac4700dcea721347
dimmaryanto93 <software.dimas_m93@icloud.com>
2019-01-03 12:50:47 +0700

    elastic search on docker

---
+++ src/main/java/com/maryanto/dimas/example/ElasticsearchExampleApplication.java
@@ -0,0 +1,14 @@
+package com.maryanto.dimas.example;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ElasticsearchExampleApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ElasticsearchExampleApplication.class, args);
+    }
+
+}
+
---
+++ src/test/java/com/maryanto/dimas/example/ElasticsearchExampleApplicationTests.java
@@ -0,0 +1,17 @@
+package com.maryanto.dimas.example;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class ElasticsearchExampleApplicationTests {
+
+    @Test
+    public void contextLoads() {
+    }
+
+}
+
